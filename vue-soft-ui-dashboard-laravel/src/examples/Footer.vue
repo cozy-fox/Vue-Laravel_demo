c4cfa0c6d896c8686c37ad8997c7364c73615097
cozy-fox <peterpaker0325@gmail.com>
2024-12-16 12:41:54 -0600

    initial commit

---
+++ vue-soft-ui-dashboard-laravel/src/examples/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="py-3 footer">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="mb-4 col-lg-6 mb-lg-0">
+          <div
+            class="text-sm text-center copyright text-muted"
+            :class="this.$store.state.isRTL ? 'text-lg-end' : 'text-lg-start'"
+          >
+            ©
+            {{ new Date().getFullYear() }}, made with
+            <i class="fa fa-heart"></i> by
+            <a
+              href="https://cozyfox-portfolio.web.app/"
+              class="font-weight-bold"
+              target="_blank"
+              >Cozy_Fox</a
+            >
+            for a better web.
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul
+            class="nav nav-footer justify-content-center justify-content-lg-end"
+          >
+            <li class="nav-item">
+              <a
+                href="https://cozyfox-portfolio.web.app/"
+                class="nav-link text-muted"
+                target="_blank"
+                >About Us</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "app-footer",
+};
+</script>
---
+++ vue-soft-ui-dashboard-laravel/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transpileDependencies: [
+      'birpc',
+      '@vue/devtools-kit',
+    ],
+  };
+  
